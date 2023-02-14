62793cd08efebf8ea83fc53063843d6cd76b8858
Jake-JD <112710032+Jake-JD@users.noreply.github.com>
2023-02-14 00:48:28 +0000

    Add files via upload

---
+++ StringEdit.py
@@ -0,0 +1,14 @@
+alltext = open("stringchange.txt", "r+")
+print(alltext.readable())
+newtext = open("newchanged.txt", "w")
+for employee in alltext.readlines():
+    x = employee.replace("Change Mew", "To This")
+    # replace "TEXT TO CHANGE"
+    # The second "" can be entered with a different string, and it will replace the first string for the second
+    # x = employee.replace("TEXT TO CHANGE","Apple") --> "Apple" will replace "TEXT TO CHANGE"
+
+    newtext.write(x)
+    print(x)
+
+alltext.close()
+newtext.close()
